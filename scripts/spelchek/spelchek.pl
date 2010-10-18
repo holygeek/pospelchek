2c975271af2b19bff36172535e9fd5d9e569dcb7
nazri <ayiehere@gmail.com>
2010-10-18 11:37:28 +0800

    Add scripts/spelchek/check_po_sanity.pl

--- scripts/spelchek/spelchek.pl
+++ scripts/spelchek/spelchek.pl
@@ -20,6 +20,7 @@ use Term::ReadKey;
 
 use lib "$Bin";
 use Spelchek;
+use PO;
 
 my %opts;
 my $valid_option = getopts('sued:w:', \%opts);
@@ -341,48 +342,10 @@ sub get_po_line {
 
 	my $po_file = "$LANGUAGE.po";
 
-	open my $PO_FILE, '<', $po_file
-		or die "Could not open $po_file: $OS_ERROR";
-	my $line_no = 0;
-	my $msgid_line_no = 0;
-	my $msgstr_line_no = 0;
-	my $wanted_line_no;
-	while (my $line = <$PO_FILE>) {
-		$line_no += 1;
-		if ($line =~ /^msgid/) {
-			$msgid_line_no = $line_no;
-			chomp $line;
-			$line =~ s/^msgid "//;
-			$line =~ s/"$//;
-			my $msgid = $line;
-			while (my $nextline = <$PO_FILE>) {
-				$line_no += 1;
-				chomp $nextline;
-				if ($nextline !~ /^msgstr/) {
-					$nextline =~ s/^"//;
-					$nextline =~ s/"$//;
-					$msgid .= $nextline;
-				} else {
-					$msgstr_line_no = $line_no;
-					if ($msgid_needle eq "\"$msgid\"") {
-						debug "NEEDLE \n[$msgid_needle]\n";
-						debug "MSGID  \n[$msgid]\n";
-						debug "line_no is [$line_no]\n";
-						if (defined $for_msgstr) {
-							debug("returning for msgstr: $msgstr_line_no\n");
-							return $msgstr_line_no;
-						} else {
-							debug("returning for msgsid: $msgid_line_no\n");
-							return $msgid_line_no;
-						}
-					}
-					last;
-				}
-			}
-		}
+	my $line_number = PO::get_po_line($po_file, $msgid_needle, $for_msgstr);
+	if ($line_number == 0) {
+		die "Couldn't get line no for msgid '$msgid_needle' in $po_file";
 	}
-
-	die "Couldn't get line no for msgid '$msgid_needle' in $po_file";
 }
 
 sub edit_po_file {
